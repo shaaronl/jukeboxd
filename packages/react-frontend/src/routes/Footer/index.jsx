9fb6336fed6ed55830de9e08c67d14f5e2934a73
BraedenAlonge <balonge@calpoly.edu>
2024-05-21 16:16:28 -0700

    added to root

--- packages/react-frontend/src/routes/Footer/index.jsx
+++ packages/react-frontend/src/routes/Footer/index.jsx
@@ -3,26 +3,26 @@ import {Container, Wrapper, Row, Column, Link, Title} from './Styles/Footer'
 
 export default function Footer(){
     return(
-        <Container {...restProps}>{children}</Container>
+        <Container>{children}</Container>
     );
 }
 
-Footer.Wrapper = function FooterWrapper({children, ...restProps}) {
-    return <Wrapper {...restProps}>{children}</Wrapper>
+Footer.Wrapper = function FooterWrapper({children}) {
+    return <Wrapper>{children}</Wrapper>
 }
 
-Footer.Wrapper = function FooterWrapper({children, ...restProps}) {
-    return <Row {...restProps}>{children}</Row>
+Footer.Wrapper = function FooterWrapper({children}) {
+    return <Row >{children}</Row>
 }
 
-Footer.Wrapper = function FooterWrapper({children, ...restProps}) {
-    return <Column {...restProps}>{children}</Column>
+Footer.Wrapper = function FooterWrapper({children}) {
+    return <Column>{children}</Column>
 }
 
-Footer.Wrapper = function FooterWrapper({children, ...restProps}) {
-    return <Link {...restProps}>{children}</Link>
+Footer.Wrapper = function FooterWrapper({children}) {
+    return <Link >{children}</Link>
 }
 
-Footer.Wrapper = function FooterWrapper({children, ...restProps}) {
-    return <Title {...restProps}>{children}</Title>
+Footer.Wrapper = function FooterWrapper({children}) {
+    return <Title>{children}</Title>
 }
--- packages/react-frontend/src/routes/Root.jsx
+++ packages/react-frontend/src/routes/Root.jsx
@@ -1,5 +1,6 @@
 import "../index.css";
 import Navbar from "./Navbar";
+import FooterContainer from "./Footer/container";
 export default function Root() {
     return (
         <div class ="loading-text">
@@ -7,6 +8,8 @@ export default function Root() {
           <h1 id = "logo-main">JUKEBOXD</h1>
           <h2>Track music you've listened to.</h2>
           <h2>Tell your friends what's good. </h2>  
+          <FooterContainer />
         </div>
+        
     );
   }
