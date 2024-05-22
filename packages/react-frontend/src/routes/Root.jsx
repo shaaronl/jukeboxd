73c748fbce31eb6b0aaf7c22ec474981191c7753
BraedenAlonge <balonge@calpoly.edu>
2024-05-22 01:37:44 -0700

    Added header. Still working on font.

--- packages/react-frontend/src/routes/Root.jsx
+++ packages/react-frontend/src/routes/Root.jsx
@@ -7,6 +7,34 @@ export default function Root() {
           <h1 id = "logo-main">JUKEBOXD</h1>
           <h2>Track music you've listened to.</h2>
           <h2>Tell your friends what's good. </h2>  
+          <footer>
+                <div className="footer-container">
+                <div className="footer-column">
+                        <h3>Help</h3>
+                        <a href="#">Story</a>
+                        <a href="#">Etc</a>
+                    </div>
+                    <div className="footer-column">
+                        <h3>About Us</h3>
+                        <a href="#">Story</a>
+                        <a href="#">Etc</a>
+                    </div>
+                    <div className="footer-column">
+                        <h3>FAQs</h3>
+                        <a href="#">Click here</a>
+                    </div>
+                    <div className="footer-column">
+                        <h3>Terms and Conditions</h3>
+                        <a href="#">Story</a>
+                        <a href="#">Etc</a>
+                    </div>
+                    <div className="footer-column">
+                        <h3>Contact Us</h3>
+                        <a href="#">Facebook</a>
+                        <a href="#">Instagram</a>
+                    </div>
+                </div>
+            </footer>
         </div>
     );
   }
