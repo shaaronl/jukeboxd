6acdaae7f1bddd28cea6913476aeba2ecd44b52d
shaaronl <sharonliang617@gmail.com>
2024-05-22 20:08:49 -0700

    users work, changed to get all reviews for album and not just one

--- packages/backend/models/reviewsSchema.js
+++ packages/backend/models/reviewsSchema.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 
 const ReviewsSchema = new mongoose.Schema(
     {
-        "review_id": {type: String, unique:true},
         "written_by":{type: ObjectId},
         "rating": Number,
         "content": String,
