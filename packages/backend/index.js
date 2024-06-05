42c435c6a7fde7d8b1c56592474da04151775876
Yakkaya <yakkaya@calpoly.edu>
2024-06-04 18:04:06 -0700

    Fixed port

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -7,6 +7,9 @@ import User from "./models/userSchema.js";
 import Reviews from "./models/reviewsSchema.js";
 import { authenticateUser } from "./auth.js";
 
+import dotenv from "dotenv";
+dotenv.config();
+
 const app = express();
 const port = 8000;
 
@@ -19,9 +22,9 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(
-    `Example app listening at http://localhost:${port}`
+    "Example app listening at http://jukeboxd-music.azurewebsites.net "
   );
 });
 
